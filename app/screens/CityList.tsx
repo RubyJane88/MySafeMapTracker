8d3a7acae487dc1569b10d323630fd2c335db64e
Ruby Jane Cabagnot <rubyjane88@gmail.com>
2020-03-10 22:36:28 +0100

    Initial commit

---
+++ app/screens/CityList.tsx
@@ -0,0 +1,15 @@
+import React, {FC, useEffect} from 'react';
+import {View, StyleSheet, Alert} from 'react-native';
+
+const CityList: FC = () => {
+    useEffect(() => {
+        console.log('alert')
+    });
+
+    return (
+        <View>
+        </View>
+    )
+};
+
+export default CityList;
